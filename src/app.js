d43cd17474181117be68f14d0d55e5c8efd36fb3
Tanuj <tanujarora1515@gmail.com>
2025-10-12 23:16:07 +0530

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -9,10 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: "http://localhost:3000",
     credentials: true
-}));
-
+}))
 app.use(cookieParser());
 app.use(fileUpload());
 
